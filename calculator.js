780689de209352fc98bf7babb8daeffb03ed07eb
nwhip001 <115032064+nwhip001@users.noreply.github.com>
2022-10-31 11:32:20 -0400

    added basic functions to calculator.js

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function operate(num1, num2) {
+
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    if (num2 === 0) {
+        return 'You cannot divide by zero.';
+    }
+    else {
+        return num1 / num2;
+    }
+}
